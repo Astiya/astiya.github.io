8f2671705811ce0d53fa1f68b16bbbbab2fe35d7
Anastasia Ivanova <71979652+Astiya@users.noreply.github.com>
2020-09-28 22:32:40 +0300

    Update index.js

--- hw1/js/index.js
+++ hw1/js/index.js
@@ -5,10 +5,12 @@ function myFunction() {
      ckd2 = document.getElementById('ckd');
 	 var label2;
 	 label2 = document.getElementById('lbl');
+	 
 	if (ckd2.checked) {
        label2.innerHTML = "Happy"
 	   path2.style.transform = "";
 	}
+	
 	else{
 		label2.innerHTML = "Sad";
 		path2.style = "transform:scale(1,-1); transform-origin: 50% 70%;"
