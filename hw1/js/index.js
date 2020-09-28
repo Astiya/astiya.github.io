f1ff357c67f3367057dcd90f860a9e176d89a082
Anastasia Ivanova <71979652+Astiya@users.noreply.github.com>
2020-09-28 22:28:42 +0300

    Update index.js

--- hw1/js/index.js
+++ hw1/js/index.js
@@ -1,2 +1,16 @@
-const g = document.querySelector('g');
-const circle = document.querySelector('circle');
+function myFunction() {
+	 var path2;
+     path2 = document.getElementById('pth');
+	 var ckd2;
+     ckd2 = document.getElementById('ckd');
+	 var label2;
+	 label2 = document.getElementById('lbl');
+	if (ckd2.checked) {
+       label2.innerHTML = "Happy"
+	   path2.style.transform = "";
+	}
+	else{
+		label2.innerHTML = "Sad";
+		path2.style = "transform:scale(1,-1); transform-origin: 50% 70%;"
+	}
+}
