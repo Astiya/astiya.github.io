a45430bcfc68fcfd827536985889cc719f71a743
Anastasia Ivanova <71979652+Astiya@users.noreply.github.com>
2020-10-26 17:41:53 +0300

    Update script.js

--- hw2/js/script.js
+++ hw2/js/script.js
@@ -60,8 +60,8 @@ loadData().then(data => {
     
     // Part 2: получитe все уникальные значения из поля 'region' при помощи d3.nest и установите их как 'domain' цветовой шкалы
     
-	let regions = d3.nest().key(function(d){return d['region'];}).entries(data);
-	
+	let regions = d3.nest().key(function(d){return d.region;}).entries(data);
+	console.log(regions);
     color.domain(regions);
 
     d3.select('.slider').on('change', newYear);
